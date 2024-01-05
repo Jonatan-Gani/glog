058aaa39b2a2a15506ab353330949fde7ac7500b
Constantine Ivanov <you@example.com>
2024-01-05 16:27:44 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='glog',
+    version='2',
+    packages=find_packages(),
+    description='Advanced logging solution',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Jonatan Gani',
+    author_email='Jonatangani@protonmail.com',
+    url='https://github.com/Jonatan-Gani/glog',
+    # Add other parameters as needed
+    install_requires=[
+        'psutil'
+    ]
+)
